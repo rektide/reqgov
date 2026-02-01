354a19c28b97f034f65de7b1a21db87687596891
rektide <rektide+scm@voodoowarez.com>
2026-01-31 20:35:55 -0500

    Implement ChainedEnricher and EnricherPresets for flexible span composition

--- src/origin_limiter.rs
+++ src/origin_limiter.rs
@@ -162,6 +162,76 @@ impl SpanEnricher for SmootherEnricher {
     }
 }
 
+pub struct ChainedEnricher {
+    enrichers: Vec<Arc<dyn SpanEnricher + Send + Sync>>,
+}
+
+impl SpanEnricher for ChainedEnricher {
+    fn enrich(&self, span: &Span, context: &SpanContext) {
+        for enricher in &self.enrichers {
+            if enricher.is_enabled() {
+                enricher.enrich(span, context);
+            }
+        }
+    }
+}
+
+impl ChainedEnricher {
+    pub fn new() -> Self {
+        Self {
+            enrichers: Vec::new(),
+        }
+    }
+
+    pub fn with_enricher<E: SpanEnricher + 'static>(mut self, enricher: E) -> Self {
+        self.enrichers.push(Arc::new(enricher));
+        self
+    }
+
+    pub fn with_enrichers<E>(mut self, enrichers: Vec<E>) -> Self {
+        for enricher in enrichers {
+            self.enrichers.push(Arc::new(enricher));
+        }
+        self
+    }
+}
+
+pub struct EnricherPresets;
+
+impl EnricherPresets {
+    pub fn minimal() -> Box<dyn SpanEnricher + Send + Sync> {
+        Box::new(MinimalSpanEnricher)
+    }
+
+    pub fn standard() -> Box<dyn SpanEnricher + Send + Sync> {
+        Box::new(StandardSpanEnricher)
+    }
+
+    pub fn detailed() -> Box<dyn SpanEnricher + Send + Sync> {
+        Box::new(DetailedSpanEnricher)
+    }
+
+    pub fn production() -> Box<dyn SpanEnricher + Send + Sync> {
+        ChainedEnricher::new()
+            .with_enricher(SmootherEnricher)
+            .with_enricher(StandardSpanEnricher)
+            .build()
+    }
+
+    pub fn debug() -> Box<dyn SpanEnricher + Send + Sync> {
+        ChainedEnricher::new()
+            .with_enricher(SmootherEnricher)
+            .with_enricher(DetailedSpanEnricher)
+            .build()
+    }
+
+    pub fn custom(enrichers: Vec<Arc<dyn SpanEnricher + Send + Sync>>) -> Box<dyn SpanEnricher + Send + Sync> {
+        ChainedEnricher::new()
+            .with_enrichers(enrichers)
+            .build()
+    }
+}
+
 pub struct DetailedSpanEnricher;
 
 impl SpanEnricher for DetailedSpanEnricher {
